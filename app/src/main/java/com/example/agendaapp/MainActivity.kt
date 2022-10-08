f69e44543875d3b5fd9c5204c15f24deddb2c571
jrodriguezs2020 <javiisalaas97@gmail.com>
2022-10-08 17:21:48 +0200

    feat: new starting screen, menu and options

--- app/src/main/java/com/example/agendaapp/MainActivity.kt
+++ app/src/main/java/com/example/agendaapp/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.agendaapp
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public fun openApp(view: View?) {
+        setContentView(R.layout.activity_menu)
     }
 }
